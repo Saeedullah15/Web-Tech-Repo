0bd80f6103ec27f0b42f97ae3fe419aad327f36c
Saeedullah15 <saeedullahazim65@gmail.com>
2023-06-17 20:56:49 +0600

    testing

---
+++ test/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+    <div style="text-align: center;">
+        <form action="second.php" method="post">
+            Name: <input type="text" name="name" id="name"><br><br>
+            Email: <input type="email" name="email" id="email"><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ test/second.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+
+<body>
+    <div style="text-align: center;">
+        Welcome
+        <?php echo $_POST["name"] ?><br>
+        Your email is
+        <?php echo $_POST["email"] ?>
+    </div>
+</body>
+
+</html>
